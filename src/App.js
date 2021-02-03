e198c0865f432e94d917417cfafe1a1829f835e7
HongYeseul <ghd8119@naver.com>
2021-02-03 11:44:58 +0900

    영화 카드 만들기

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axois from 'axios';
+import Movie from './Movie';
+import './App.css'
+
+function MakeLoading({loading, loadingModify}){
+  let getMovies = async () =>{
+    const {
+      data : {
+        data : {movies},
+      },
+    } = await axois.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating');
+    loadingModify({movies, isLoading:false})
+  }
+  useEffect(()=>{
+    getMovies()
+  });
+  return (
+    <section className="container">
+      {
+        loading.isLoading 
+        ?<div className="loader"> <span className="loader__text">"Loading..." </span> </div> 
+        : loading.movies.map((movie) =>{
+          return (
+          <div className="movies">
+            {<Movie
+              key = {movie.id}
+              id = {movie.id}
+              year = {movie.year}
+              title={movie.title}
+              summary = {movie.summary}
+              poster = {movie.medium_cover_image}
+              genres = {movie.genres}
+          />}
+          </div>
+          );
+        })
+      }
+    </section>
+    );
+}
 
 function App() {
+  let [loading, loadingModify] = useState({
+    isLoading : true,
+    movies : []
+  })
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <MakeLoading loading={loading} loadingModify={loadingModify}/>
     </div>
   );
 }
